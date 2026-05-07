12c4b6adb5e710041253380111b8aca55a54a1be
shreyas kawale <shreyas.kawale@noovosoft.com>
2026-05-07 22:25:09 +0530

    Fix name issue

--- assets/urls.py
+++ assets/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DriverViewSet, VehicleViewSet, VehicleDocumentViewSet, ExpenseViewSet
-router.register(r'expenses', ExpenseViewSet, basename='expense')
-
 router = DefaultRouter()
 router.register(r'vehicles', VehicleViewSet, basename='vehicle')
 router.register(r'drivers', DriverViewSet, basename='driver')
 router.register(r'vehicle-documents', VehicleDocumentViewSet, basename='vehicle-document')
+router.register(r'expenses', ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('', include(router.urls)),
